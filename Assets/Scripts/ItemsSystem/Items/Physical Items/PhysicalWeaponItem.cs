935fdf9b0a853a9da6110341cd588182673f3774
Denis Litvinets <denis.litvinets@gmail.com>
2023-02-22 16:56:31 +0300

    fix weapon clipping

--- Assets/Scripts/ItemsSystem/Items/Physical Items/PhysicalWeaponItem.cs
+++ Assets/Scripts/ItemsSystem/Items/Physical Items/PhysicalWeaponItem.cs
@@ -4,6 +4,9 @@ namespace ItemsSystem
 {
     public abstract class PhysicalWeaponItem : PhysicalItemBase
     {
+        [SerializeField] private int _interactableLayer;
+        [SerializeField] private int _weaponLayer;
+
         public bool CanAttack { get; protected set; } = true;
         public float SwayIntensity { get; protected set; }
         public float SwaySmooth { get; protected set; }
@@ -24,6 +27,7 @@ namespace ItemsSystem
             GetComponent<Rigidbody>().isKinematic = true;
             GetComponent<BoxCollider>().enabled = false;
             InPlayerHands = true;
+            SetItemLayer(_weaponLayer);
         }
 
         public virtual void Attack()
@@ -34,6 +38,14 @@ namespace ItemsSystem
         public virtual void Unequip()
         {
             InPlayerHands = false;
+            SetItemLayer(_interactableLayer);
+        }
+
+        private void SetItemLayer(int newLayer)
+        {
+            transform.gameObject.layer = newLayer;
+            foreach (Transform weaponPart in transform)
+                weaponPart.gameObject.layer = newLayer;
         }
     }
 }
